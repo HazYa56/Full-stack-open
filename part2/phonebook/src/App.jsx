88bb66f1ac36ab8af689a2963cd1d5225edb4044
YAGOUB <yagoub27@duck.com>
2024-07-10 18:33:47 +0100

    Exercise-2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,26 +3,35 @@ import { useState } from 'react'
 const Person = ({ name, number }) => <p>{ name } { number }</p>
 const App = () => {
   /* States */
+  const [search, setSearch] = useState('')
   const [newPerson, setNewPerson] = useState({
     name:'',
     number: ''
   })
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Arto Hellas', number: '040-123456', id: 56329 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 23454 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 85693 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 89862 }
   ])
+  const personsSearched = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
 
   /* Event handlers */
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
   const handleNameChange = (event) => {
     setNewPerson({ 
       name: event.target.value,
-      number: newPerson.number
+      number: newPerson.number,
+      id: Math.round(Math.random()*1e5)
     })
   }
   const handleNumberChange = (event) => {
     setNewPerson({ 
       name: newPerson.name,
-      number: event.target.value
+      number: event.target.value,
+      id: Math.round(Math.random()*1e5)
     })
   }
   const handlePersonAdd = (event) => {
@@ -49,6 +58,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with <input value={search} onChange={handleSearchChange} /></p>
+      <h2>add a new</h2>
       <form onSubmit={handlePersonAdd}  >
         <div>
           name: <input value={newPerson.name} onChange={handleNameChange} />
@@ -61,7 +72,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person key={person.number} name={person.name} number={person.number} />)}
+      {personsSearched.map(person => <Person key={person.id} name={person.name} number={person.number} />)}
     </div>
   )
 }
