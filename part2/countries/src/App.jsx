2e8fd703a5fcc4862ab5d1f44d6319a1cc4ff64e
YAGOUB <yagoub27@duck.com>
2024-07-13 03:57:00 +0100

    Exercise-2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -29,6 +29,14 @@ const App = () => {
     setCountry(event.target.value)
   }
   const showCountry = (result, index) => {
+    const APIkey = import.meta.env.VITE_SOME_KEY
+    const weather = (API_key) => {
+      axios.get(`https://${API_key}@api.meteomatics.com/${new Date().toISOString()}/t_2m:C,wind_speed_10m:ms/${result[index].capitalInfo.latlng[0]},${result[index].capitalInfo.latlng[1]}/json`)
+      .then(response => {
+        const values = response.data.map(data => data.coordinates[0].dates[0].value)
+        return <p>temperature {values[0]} Celcius<br />wind {values[1]} m/s</p>
+      })
+    }
     return (
       <>
         <h1>{result[index].name.common}</h1>
@@ -36,6 +44,7 @@ const App = () => {
         <h3>languages</h3>
         <ul>{Object.values(result[index].languages).map(lang => <li key={lang}>{lang}</li>)}</ul>
         <img src={result[index].flags.svg} style={{ width: "150px", height: "150px"}} />
+        {weather(APIkey)}
       </>
     )
   }
