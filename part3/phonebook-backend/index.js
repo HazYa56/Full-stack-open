c0a72d8cf4138bdb296b9508f051464fb4c7e5e7
YAGOUB <yagoub27@duck.com>
2024-07-17 15:40:08 +0100

    Exercise-3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,26 +3,26 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": "56329",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "23454",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "85693",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "89862",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get("/api/persons", (req, res) => {
@@ -32,6 +32,13 @@ app.get("/api/persons", (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
   persons = persons.filter(person => person.id != req.params.id).concat()
   res.status(204).end()
+})
+
+app.post('/api/persons/', (req, res) => {
+  const newPerson = req.body
+  newPerson.id = Math.round(Math.random()*1e5).toString()
+  persons = persons.concat(newPerson)
+  res.json(newPerson)
 })
 
 app.get("/api/info", (req, res) =>{
